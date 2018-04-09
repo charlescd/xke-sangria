9e95c76ecbb3061164c60d2478b85911ba53bb53
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2018-04-09 12:03:35 +0200

    Remove InputObjectType on Author

--- src/main/scala/fr/xebia/xke/sangria/models/author/Author.scala
+++ src/main/scala/fr/xebia/xke/sangria/models/author/Author.scala
@@ -3,15 +3,13 @@ package fr.xebia.xke.sangria.models.author
 import java.util.UUID
 
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
-import sangria.macros.derive.{deriveInputObjectType, deriveObjectType}
-import fr.xebia.xke.sangria.graphql.Scalar._
+import sangria.macros.derive.deriveObjectType
 
 case class Author(id: UUID, lastName: String, firstName: String)
 
 object Author {
   //GraphQL
   implicit val AuthorType = deriveObjectType[Unit, Author]()
-  implicit val AuthorInputType = deriveInputObjectType[Author]()
 
   // REST
   implicit val decoder = deriveDecoder[Author]
